6561856ed7bd780d21f682e4ace11f482059648d
hamza0476 <160503217+hamza0476@users.noreply.github.com>
2025-11-09 11:57:30 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/core';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.2501b7555345424a95a188fce5ad97c5',
+appId: 'com.hamza.expensetracker',
   appName: 'expense-wiz-38',
   webDir: 'dist',
   server: {
